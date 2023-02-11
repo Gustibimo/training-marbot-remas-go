dbf09d830310d8f390a53fe31285dbf82b70a02c
gstbimo <gusti.bimo@mekari.com>
2023-02-11 23:27:21 +0700

    delete unused import

--- main.go
+++ main.go
@@ -4,28 +4,10 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
-
-	// "github.com/Gustibimo/training-marbot-remas-go.git/domain"
-	// "github.com/Gustibimo/training-marbot-remas-go.git/persistence"
 	"github.com/Gustibimo/training-marbot-remas-go.git/presentation"
 )
 
 func main() {
-	// var userRepo persistence.Users
-	// userRepo.FindUserByFirstName("Rebecca")
-
-	// var user persistence.Users
-
-	// userService := domain.NewUserService(&user)
-
-	// result, err := userService.FindUserByFirstName("Rebecca")
-	// if err != nil {
-	// 	fmt.Println("not found")
-	// }
-	// fmt.Print(result)
-
-	// res := userService.FindAllUserFirstName()
-	// fmt.Println(res)
 
 	http.HandleFunc("/", presentation.Root)
 	http.HandleFunc("/users/", presentation.GetUserByFirstName)
@@ -37,7 +19,5 @@ func main() {
 	} else if err != nil {
 		fmt.Printf("error starting server")
 	}
-	
-
 
 }
